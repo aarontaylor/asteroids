e27ca020dac814c86ec84b7c39d4e1522323e089
Aaron Taylor <aarontaylor@users.noreply.github.com>
2024-11-09 11:16:59 -0500

    Initial Asteroids Game Setup

---
+++ main.py
@@ -0,0 +1,14 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+    
+# Using the special variable __name__
+if __name__ == "__main__":
+    main()
