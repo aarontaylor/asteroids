5e824df9d44fcfdc80b0be5280571f4a970b07dc
Aaron Taylor <aarontaylor@users.noreply.github.com>
2024-11-09 11:26:10 -0500

    Create game display

--- main.py
+++ main.py
@@ -5,9 +5,18 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
     print("Starting asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(000)
+        pygame.display.flip()
     
 # Using the special variable __name__
 if __name__ == "__main__":
