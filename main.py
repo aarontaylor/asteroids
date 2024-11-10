e736d1f15d04c141065f1ffaecb093a8d8d7ae66
Aaron Taylor <aarontaylor@users.noreply.github.com>
2024-11-10 09:25:49 -0500

    Used Pygame Groups to simplify updates and drawing

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ def main():
     #set frame rate
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -18,10 +24,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
+        
 
         screen.fill("black")
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # limit framerate to 60 FPS
