8fe25a84e7a40b4e32fc3461833f45195b02653f
Aaron Taylor <aarontaylor@users.noreply.github.com>
2024-11-10 10:10:03 -0500

    Added asteroid collision detection with player

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
+import sys
 import pygame
 from constants import *
 from player import Player
@@ -33,6 +31,11 @@ def main():
         
         for obj in updatable:
             obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
         
 
         screen.fill("black")
